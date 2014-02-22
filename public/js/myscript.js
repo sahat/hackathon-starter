f9f32e9f26f2c2da7c2a77ee6fdd4a3639008885
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-02-22 17:08:44 -0500

    Made it work

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 		return val;
 	}
 
+	var data = new FormData();
+	data.append("thing", "thing");
+	console.log(data);
+
 	var myCodeMirror = CodeMirror(document.getElementById("editor"), {
 		value: generateCode(),
 		lineNumbers: true,
