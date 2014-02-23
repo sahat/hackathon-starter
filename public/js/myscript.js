0164a5a1e386c46a001b7082139948ff1510dd39
Anas Ambri <anas.ambri@gmail.com>
2014-02-22 23:57:51 -0500

    Adding goangular to project, finally

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,58 +1,20 @@
 $(document).ready(function(){
+    function generateCode(){
+	var val = "function myScript(){" +
+		"\n\treturn 100;" +
+		"\n}";				
+	return val;
+    }
 
-	function generateCode(){
-		var val = "function myScript(){" +
-			"\n\treturn 100;" +
-			"\n}";				
-		return val;
-	}
+    var data = new FormData();
+    data.append("thing", "thing");
+    console.log(data);
 
-	var data = new FormData();
-	data.append("thing", "thing");
-	console.log(data);
-
-	var myCodeMirror = CodeMirror(document.getElementById("editor"), {
-		value: generateCode(),
-		lineNumbers: true,
-		matchBrackets: true,
-		mode:  "javascript"
-	});
-
-    var chat = angular.module('Chat', ['goangular']);
-    chat.config(function($goConnectionProvider) {
-	    //TODO replace with app.locals.connectUrl
-            $goConnectionProvider.$set('https://goinstant.net/e106cb106c84/mchacks');
-	});
-    chat.controller('ChatCtrl', function($scope, $goKey) {
-            $scope.messages = $goKey('messages').$sync();
-
-            $scope.messages.$on('add', {
-		local: true,
-		listener: scrollOn
-            });
-
-            $scope.messages.$on('ready', scrollOn);
-
-            $scope.sendMessage = function() {
-		if(!$scope.newMessage) {
-		    return;
-		}
-
-		$scope.messages.$add({
-		    content: $scope.newMessage,
-		    author: $scope.author
-		}).then(function() {
-		    $scope.$apply(function() {
-			$scope.newMessage = '';
-		    });
-		});
-            };
-
-            function scrollOn() {
-		setTimeout(function() {
-		    $('.table-wrapper').scrollTop($('.table-wrapper').children().height());
-		}, 0);
-            }
-	});
-
+    var myCodeMirror = CodeMirror(document.getElementById("editor"), {
+	value: generateCode(),
+	lineNumbers: true,
+	matchBrackets: true,
+	mode:  "javascript"
+    });
 });
+
