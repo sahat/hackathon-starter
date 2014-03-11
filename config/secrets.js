074177627bc5c4baaf29a247fde18f7ec5973e0d
Sahat Yalkabov <sakhat@gmail.com>
2014-03-11 14:15:04 -0400

    Added environment variables. Fixes #100

--- config/secrets.js
+++ config/secrets.js
@@ -1,111 +1,106 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/test',
+  db: process.env.MONGODB|| 'mongodb://localhost:27017/test',
 
-  sessionSecret: "Your Session Secret goes here",
+  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
   localAuth: true,
 
   mailgun: {
-    login: 'Your Mailgun SMTP Username',
-    password: 'Your Mailgun SMTP Password'
+    login: process.env.MAILGUN_LOGIN || 'Your Mailgun SMTP Username',
+    password: process.env.MAILGUN_PASSWORD || 'Your Mailgun SMTP Password'
   },
 
   sendgrid: {
-    user: 'Your SendGrid Username',
-    password: 'Your SendGrid Password'
-  },
-
-  gmail: {
-    user: 'Your Gmail Username',
-    password: 'Your Gmail Password'
+    user: process.env.SENDGRID_USER || 'Your SendGrid Username',
+    password: process.env.SENDGRID_PASSWORD || 'Your SendGrid Password'
   },
 
   nyt: {
-    key: 'Your New York Times API Key'
+    key: process.env.NYT_KEY || 'Your New York Times API Key'
   },
 
   lastfm: {
-    api_key: 'Your API Key',
-    secret: 'Your API Secret'
+    api_key: process.env.LASTFM_KEY || 'Your API Key',
+    secret: process.env.LASTFM_SECRET || 'Your API Secret'
   },
 
   facebookAuth: true,
   facebook: {
-    clientID: 'Your App ID',
-    clientSecret: 'Your App Secret',
+    clientID: process.env.FACEBOOK_ID || 'Your App ID',
+    clientSecret: process.env.FACEBOOK_SECRET || 'Your App Secret',
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
 
   githubAuth: true,
   github: {
-    clientID: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
+    clientID: process.env.GITHUB_ID || 'Your Client ID',
+    clientSecret: process.env.GITHUB_SECRET || 'Your Client Secret',
     callbackURL: '/auth/github/callback',
     passReqToCallback: true
   },
 
   twitterAuth: true,
   twitter: {
-    consumerKey: 'Your Consumer Key',
-    consumerSecret: 'Your Consumer Secret',
+    consumerKey: process.env.TWITTER_KEY || 'Your Consumer Key',
+    consumerSecret: process.env.TWITTER_SECRET  || 'Your Consumer Secret',
     callbackURL: '/auth/twitter/callback',
     passReqToCallback: true
   },
 
   googleAuth: true,
   google: {
-    clientID: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
+    clientID: process.env.GOOGLE_ID || 'Your Client ID',
+    clientSecret: process.env.GOOGLE_SECRET || 'Your Client Secret',
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
   },
 
   linkedinAuth: true,
   linkedin: {
-    clientID: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
+    clientID: process.env.LINKEDIN_ID || 'Your Client ID',
+    clientSecret: process.env.LINKEDIN_SECRET || 'Your Client Secret',
     callbackURL: '/auth/linkedin/callback',
     scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
     passReqToCallback: true
   },
 
   steam: {
-    apiKey: 'Your Steam API Key'
+    apiKey: process.env.STEAM_KEY || 'Your Steam API Key'
   },
 
   twilio: {
-    sid: 'Your Twilio SID',
-    token: 'Your Twilio token'
+    sid: process.env.TWILIO_SID || 'Your Twilio SID',
+    token: process.env.TWILIO_TOKEN || 'Your Twilio token'
   },
 
   clockwork: {
-    apiKey: 'Your Clockwork SMS API Key'
+    apiKey: process.env.CLOCKWORK_KEY || 'Your Clockwork SMS API Key'
   },
 
   tumblr: {
-    consumerKey: 'Your Consumer Key',
-    consumerSecret: 'Your Consumer Secret',
+    consumerKey: process.env.TUMBLR_KEY || 'Your Consumer Key',
+    consumerSecret: process.env.TUMBLR_SECRET || 'Your Consumer Secret',
     callbackURL: '/auth/tumblr/callback'
   },
 
   foursquare: {
-    clientId: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/foursquare/callback'
+    clientId: process.env.FOURSQUARE_ID || 'Your Client ID',
+    clientSecret: process.env.FOURSQUARE_SECRET || 'Your Client Secret',
+    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || 'http://localhost:3000/auth/foursquare/callback'
   },
 
   venmo: {
-    clientId: 'Your Venmo Client ID',
-    clientSecret: 'Your Venmo Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/venmo/callback'
+    clientId: process.env.VENMO_ID || 'Your Venmo Client ID',
+    clientSecret: process.env.VENMO_SECRET || 'Your Venmo Client Secret',
+    redirectUrl: process.env.VENMO_REDIRECT_URL || 'http://localhost:3000/auth/venmo/callback'
   },
 
   paypal: {
-    host: 'api.sandbox.paypal.com', // or api.paypal.com
-    client_id: 'Your Client ID',
-    client_secret: 'Your Client Secret',
-    returnUrl: 'http://localhost:3000/api/paypal/success',
-    cancelUrl: 'http://localhost:3000/api/paypal/cancel'
+    host: process.env.PAYPAL_HOST || 'api.sandbox.paypal.com',
+    client_id: process.env.PAYPAL_ID || 'Your Client ID',
+    client_secret: process.env.PAYPAL_SECRET || 'Your Client Secret',
+    returnUrl: process.env.PAYPAL_RETURN_URL || 'http://localhost:3000/api/paypal/success',
+    cancelUrl: process.env.PAYPAL_CANCEL_URL || 'http://localhost:3000/api/paypal/cancel'
   }
 };
