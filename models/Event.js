a965f92e553f28eb9437deead7eddffdfe518279
chrisxwan <christopher.wan@yale.edu>
2014-11-01 04:05:46 -0400

    Modify Events.js more

--- models/Event.js
+++ models/Event.js
@@ -1,1 +1,45 @@
-Event.js
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+
+var eventSchema = new mongoose.Schema({
+  title: { type: String, default: ''},
+  organizer: ObjectId,
+  description: { type: String, default: ''},
+  location: { type: String, default: ''},
+  day: { type: Date },
+  time: Number, 
+  users: Array
+
+
+/*name: { type: String, default: '' },
+    gender: { type: String, default: '' },
+    location: { type: String, default: '' },
+    website: { type: String, default: '' },
+    picture: { type: String, default: '' }*/
+});
+
+/**
+ * Hash the password for security.
+ * "Pre" is a Mongoose middleware that executes before each user.save() call.
+ */
+
+eventSchema.pre('save', function(next) {
+  var  = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+
+module.exports = mongoose.model('Event', eventSchema);
