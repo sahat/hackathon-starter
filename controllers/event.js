8ae443cdbb33bced79fab5b3de3ef53e0d346974
chrisxwan <christopher.wan@yale.edu>
2014-11-01 12:01:36 -0400

    modified events.js controller

--- controllers/event.js
+++ controllers/event.js
@@ -19,4 +19,28 @@ exports.postNewEvent = function(req, res) {
     req.assert('length', 'Yo, how long your pow wow gon\' be?').notEmpty();
     req.assert('day', 'Yo, when you wanna plan dis shiz').notEmpty();
     req.assert('users', 'Yo, why the fuck you using this app tho').notEmpty();
+
+    var errors = req.validationErrors();
+
+ 	 if (errors) {
+    	req.flash('errors', errors);
+    	return res.redirect('/new_event');
+  	}
+
+  	var event = new Event({
+   		 title: req.body.title,
+   	 	 description: req.body.description,
+   	 	 location: req.body.location,
+   	 	 day: req.body.day,
+   	 	 length: req.body.length,
+   	 	 users: req.body.users,
+   	 	 organizer: req.user
 }
+
+exports.postDeleteEvent = function(req, res, next) {
+  Event.remove({ _id: req.event.id }, function(err) {
+    if (err) return next(err);
+    req.flash('info', { msg: 'Your event has been deleted.' });
+    res.redirect('/');
+  });
+};
