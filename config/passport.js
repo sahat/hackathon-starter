77ac7f55431d1cbc08dfbcdee8c789d7eb3bce94
Sahat Yalkabov <sakhat@gmail.com>
2015-02-22 22:36:03 -0800

    Convert email to lowerCase in LocalStrategy

--- config/passport.js
+++ config/passport.js
@@ -71,6 +71,7 @@ passport.use(new InstagramStrategy(secrets.instagram,function(req, accessToken, 
  * Sign in using Email and Password.
  */
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  email = email.toLowerCase();
   User.findOne({ email: email }, function(err, user) {
     if (!user) return done(null, false, { message: 'Email ' + email + ' not found'});
     user.comparePassword(password, function(err, isMatch) {
