8a2572265c7d3b18ac7597f7a7e991517fd83fc9
Roman Storm <rstormsf@gmail.com>
2015-03-21 01:44:30 -0700

    User model doesn't seem to be used in this file
    Please confirm if I'm right

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,4 @@
 var secrets = require('../config/secrets');
-var User = require('../models/User');
 var querystring = require('querystring');
 var validator = require('validator');
 var async = require('async');
