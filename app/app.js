cc4d1b2bb7192c9c18b64ebc04e174d332f5a28b
knighthunter09 <emailforshekhar@gmail.com>
2015-03-28 17:42:02 +0800

    Updated path for including bower components

--- app/app.js
+++ app/app.js
@@ -4,8 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.view2'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
