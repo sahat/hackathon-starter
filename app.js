99969b9cea37a641169a2fa16529fb68b255bb89
Chamila Piyasena <tchamila@gmail.com>
2015-03-29 04:31:40 +0800

    refactoring unnessary code

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ var Agenda = require('agenda');
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
-var contactController = require('./controllers/contact');
 var eventsController = require('./controllers/event');
 var rsvpController = require('./controllers/rsvp');
 
@@ -119,8 +118,6 @@ app.get('/reset/:token', userController.getReset);
 app.post('/reset/:token', userController.postReset);
 app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
