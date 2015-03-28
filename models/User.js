b1079a3dce80111e31dff7ddb6191867b9d632dc
Kelum Senanayake <kelumkps@yahoo.com>
2015-03-29 02:04:31 +0800

    Adding user service and rsvp endpoint

--- models/User.js
+++ models/User.js
@@ -19,7 +19,8 @@ var userSchema = new mongoose.Schema({
     gender: { type: String, default: '' },
     location: { type: String, default: '' },
     website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+    picture: { type: String, default: '' },
+    mobileNumber: String
   },
 
   resetPasswordToken: String,
