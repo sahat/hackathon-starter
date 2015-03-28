8dc8b242a1bcae97adbe0b89deb7b4d525640857
knighthunter09 <emailforshekhar@gmail.com>
2015-03-29 01:55:04 +0800

    Integration of signup page flow 1

--- controllers/user.js
+++ controllers/user.js
@@ -96,7 +96,7 @@ exports.postSignup = function(req, res, next) {
       if (err) return next(err);
       req.logIn(user, function(err) {
         if (err) return next(err);
-        res.redirect('/');
+        res.redirect('./app/index.html');
       });
     });
   });
