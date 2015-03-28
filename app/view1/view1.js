8d835b4e4ab151a7d97ab0b7d5e72abf3cd705d5
knighthunter09 <emailforshekhar@gmail.com>
2015-03-28 21:51:19 +0800

    Frontend basic changes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,17 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'eventCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('eventCtrl',["$scope",function($scope) {
+    $scope.eventData = [
+        {"title":"Event Title1","description":"Event Descriptssssssssssssssssssssssssssssssssssssssss","eventDate": "1234","type":"To be chosen"},
+        {"title":"Event Title2","description":"Event Description2","eventDate": "1234","type":"To be chosen"},
+        {"title":"Event Title3","description":"Event Description3","eventDate": "1234","type":"To be chosen"},
+        {"title":"Event Title4","description":"Event Description4","eventDate": "12111134","type":"To be chosen"},
+        {"title":"Event Title5","description":"Event Description5","eventDate": "1234","type":"To be chosen"},
+        {"title":"Event Title6","description":"Event Description6","eventDate": "1234","type":"To be chosen"}
+    ];
 }]);
