4e32bae1198ec9b48eaa30b2d43ebeacc0ce41f6
flashsnake-so <a122178@hotmail.com>
2016-01-06 09:34:10 -0800

    added the forever start/stop script for running node in ec2

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+# Invoke the Forever module (to START our Node.js server).
+./node_modules/forever/bin/forever \
+start \
+-al forever.log \
+-ao out.log \
+-ae err.log \
+server.js
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ 
+# Invoke the Forever module (to STOP our Node.js server).
+./node_modules/forever/bin/forever stop server.js
