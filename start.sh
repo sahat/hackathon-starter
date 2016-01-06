ef32e76213c4388059ad5f4372f288670a303fd2
flashsnake-so <a122178@hotmail.com>
2016-01-06 09:43:52 -0800

    added the README In start.sh and stop.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
+# This script can be found in http://www.bennadel.com/blog/2321-how-i-got-node-js-running-on-a-linux-micro-instance-using-amazon-ec2.htm
+
 #!/bin/bash
- 
+  
 # Invoke the Forever module (to START our Node.js server).
 ./node_modules/forever/bin/forever \
 start \
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
+# This script can be found in http://www.bennadel.com/blog/2321-how-i-got-node-js-running-on-a-linux-micro-instance-using-amazon-ec2.htm
+
 #!/bin/bash
  
 # Invoke the Forever module (to STOP our Node.js server).
