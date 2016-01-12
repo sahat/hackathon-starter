ce5758e844dc7e9eea69d28b4095d12be82607be
flashsnake-so <a122178@hotmail.com>
2016-01-12 11:06:34 -0800

    fix the redirect issue after successful login

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get('/auth/instagram/callback', passport.authenticate('instagram', { failure
 });
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location', 'publish_actions'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect("/app.html");
+  res.redirect("/app.html#/campaign");
 });
 app.get('/auth/github', passport.authenticate('github'));
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
