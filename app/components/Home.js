3259e37314f2aa38faa20b9b07837803aa57b744
Genevieve <genevieve.sublette@capitalone.com>
2017-03-04 15:37:54 -0800

    add todos

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,6 +4,7 @@ import Messages from './Messages';
 import VolunteerForm from './VolunteerForm.react';
 
 class Home extends React.Component {
+  //TODO: the volunteer form should be rendered after a user selects a volunteer role to apply for.
   render() {
     return (
       <div className="container-fluid">
