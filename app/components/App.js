1444ab298de2aa2a88326b44055f8cbea4e278f9
Marri Gamard <marri@Marris-MacBook-Air.local>
2017-03-04 17:27:01 -0800

    added material design

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from './Header';
 import Footer from './Footer';
 
 class App extends React.Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div>
         <Header/>
         {this.props.children}
         <Footer/>
       </div>
+    </MuiThemeProvider>
     );
   }
 }
--- app/main.js
+++ app/main.js
@@ -5,6 +5,13 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import configureStore from './store/configureStore';
 import getRoutes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const store = configureStore(window.INITIAL_STATE);
 
