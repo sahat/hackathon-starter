1a9d3aabf1225f44304850c00fd5f8536654eb21
Alexxis Johnson <alexxis.johnson@capitalone.com>
2017-03-04 16:23:05 -0800

    Update year of copyright

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>© 2016 Company, Inc. All Rights Reserved.</p>
+        <p>© 2017 Company, Inc. All Rights Reserved.</p>
       </footer>
     );
   }
