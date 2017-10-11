de2a61d9572434ddbe9eb26defd707424c64452d
narendramanchala <mvnarendrareddy@gmail.com>
2017-10-11 19:22:54 +0530

    delete using ajax completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+    $("#updaterForm").click(function(){
+        $("#sporthide").hide();
+        $("#editform").show();
+    });
+    // Place JavaScript code here...
+    $("#updater").click(function(e){
+        e.preventDefault();
+        $.ajax({
+            method: "post",
+            url: "/updateSport",
+            data: {_csrf: $("#_csrf").val(),id: $("#id").val(), name: $("#name").val(), type: $("#type").val() }
+        })
+            .done(function( msg ) {
+                console.log(msg);
+                $('#sporthide').show();
+                $('#editform').hide();
+            });
+    })
 
-  // Place JavaScript code here...
-
+    $("#deleteAjax").click(function(){
+        $.ajax({
+            method: "post",
+            url: "/deleteSport",
+            data: {_csrf:$("#_csrf").val(), name:$("#name").val(), type:$("#type").val() }
+        })
+        .done(function( msg ) {
+            console.log(msg);
+            //$('#sporthide').show();
+            //$('#editform').hide();
+        });
+    })
 });
+
