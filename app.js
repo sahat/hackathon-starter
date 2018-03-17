31160a47c14cfe8f2a7bfd0ad93d0717980ebe88
Mohit Kumar <ethicalmohit@users.noreply.github.com>
2018-03-18 01:46:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ mongoose.connection.on('error', (err) => {
  * Express configuration.
  */
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
