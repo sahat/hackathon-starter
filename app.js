7390a7e386417ca257a23020d2f2ed17d1e04e3b
Shreedhar <shreedhar@uber.com>
2018-03-23 09:44:28 +0530

    changed to req.originalUrl as req.url is not a native express property

--- app.js
+++ app.js
@@ -106,10 +106,10 @@ app.use((req, res, next) => {
       req.path !== '/signup' &&
       !req.path.match(/^\/auth/) &&
       !req.path.match(/\./)) {
-    req.session.returnTo = req.url;
+    req.session.returnTo = req.originalUrl;
   } else if (req.user &&
       req.path === '/account') {
-    req.session.returnTo = req.url;
+    req.session.returnTo = req.originalUrl;
   }
   next();
 });
