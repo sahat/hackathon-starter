9031407e024e02cf685168f35c7c83f048fbd40f
Riley Miller <rileymiller@mymail.mines.edu>
2018-05-16 18:23:15 -0600

    up to date model paths in config/passport.js api/controllers/user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,7 +2,7 @@ const { promisify } = require('util');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const passport = require('passport');
-const User = require('../../models/User');
+const User = require('../models/User');
 
 const randomBytesAsync = promisify(crypto.randomBytes);
 
