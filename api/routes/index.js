adb79b50bf988d9791442018578d48d7d134a84a
Madison Rogers <madisonrogers@csm-wl-dhcp-225-211.mines.edu>
2018-05-17 02:57:11 -0600

    Made additions to the workoutCtrl.js file. None of it has been tested yet.

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ var userCtrl = require('../controllers/userCtrl');
 var teamCtrl = require('../controllers/teamCtrl');
 var groupCtrl = require('../controllers/groupCtrl');
 var phaseCtrl = require('../controllers/phaseCtrl');
+var workoutCtrl = require('../controllers/workoutCtrl');
 
 // userCtrl routes
 router.get('/getUser/:userid', userCtrl.getUserById);
@@ -19,7 +20,7 @@ router.get('/getTeam/:teamid', teamCtrl.getTeamById);
 router.post('/createTeam', teamCtrl.createTeam);
 router.get('/allTeams', teamCtrl.getAllTeams);
 router.put('/updateTeam/:teamid', teamCtrl.updateTeam);
-router.put('/addUserToTeam/:teamid/:userid', teamCtrl.addUser); // THIS STILL NEEDS TO BE TESTED
+router.put('/addUserToTeam/:teamid/:userid', teamCtrl.addUser);
 
 // groupCtrl routes
 router.get('/getGroup/:groupid', groupCtrl.getGroupById);
@@ -27,9 +28,9 @@ router.get('/allGroups', groupCtrl.getAllGroups);
 router.post('/createGroup/:teamid', groupCtrl.createGroup);
 router.put('/updateGroup/:groupid', groupCtrl.updateGroup);
 router.delete('/deleteGroup/:groupid', groupCtrl.deleteGroup);
-router.put('/addUserToGroup/:groupid/:userid', groupCtrl.addUser); // THIS STILL NEEDS TO BE TESTED
+router.put('/addUserToGroup/:groupid/:userid', groupCtrl.addUser);
 
-//phaseCtrl routes
+// phaseCtrl routes
 router.get('/getPhase/:phaseid', phaseCtrl.getPhaseById);
 router.get('/allPhaseTeam', phaseCtrl.getPhaseByTeam);
 //router.get('/allPhaseUser', groupCtrl.getAllPhasesByUser);
@@ -37,5 +38,11 @@ router.get('/allPhases', phaseCtrl.allPhases);
 router.put('/updatePhaseTeam/:phaseid', phaseCtrl.updatePhaseByTeam);
 router.put('/updatePhaseUser/:phaseid',phaseCtrl.updatePhaseByUser);
 router.post('/:teamid/phase', phaseCtrl.createPhaseTeam)
+
+// workoutCtrl routes
+router.put('/addExercise/:workoutid/:blockid', workoutCtrl.addExercise);
+router.post('/createWorkout/:phaseid', workoutCtrl.createWorkout);
+router.get('/getWorkout/:workoutid', workoutCtrl.getWorkout);
+router.get('/allWorkouts', workoutCtrl.getAllWorkouts);
 
 module.exports = router;
