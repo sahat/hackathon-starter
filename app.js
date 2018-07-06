97538db2f8a3e0b429248d2b0163916d2e195667
YasharF <YasharF.Git1@ydftech.com>
2018-07-05 21:34:07 -0700

    Use the JS libraries from node_modules without copying

--- app.js
+++ app.js
@@ -113,7 +113,10 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/popper.js/dist'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
 
 /**
  * Primary app routes.
