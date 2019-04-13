c52207372c07c14090a78f49e655b9e1b2aa6c60
FX-Wood <fxwood2@gmail.com>
2019-04-13 15:28:52 -0700

    Refactored most of steam api request

--- controllers/api.js
+++ controllers/api.js
@@ -306,25 +306,31 @@ exports.getSteam = async (req, res, next) => {
   const steamId = req.user.steam;
   const params = { l: 'english', steamid: steamId, key: process.env.STEAM_KEY };
   const getAsync = promisify(request.get);
-
-  // get the list of the recently played games, pick the most recent one and get its achievements
-  const getPlayerAchievements = () =>
-    getAsync({ url: 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/', qs: params, json: true })
-      .then(({ request, body }) => {
-        if (request.statusCode === 401) {
-          throw new Error('Invalid Steam API Key');
-        }
-        if (body.response.total_count > 0) {
-          params.appid = body.response.games[0].appid;
-          return getAsync({ url: 'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/', qs: params, json: true })
-            .then(({ request, body }) => {
-              if (request.statusCode === 401) {
-                throw new Error('Invalid Steam API Key');
-              }
-              return body;
-            });
-        }
-      });
+  
+  const makeURL = (domainAndPath, queryParams) => {
+    const url = new URL(domainAndPath)
+    const encodedQuery = new URLSearchParams(queryParams)
+    url.search = encodedQuery.toString()
+    return url.toString()
+  }
+    // get the list of the recently played games, pick the most recent one and get its achievements
+  const getPlayerAchievements = () => {
+    const recentGamesURL = makeURL('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/', params)
+    return axios.get(recentGamesURL)
+    .then(({ data }) => {
+      if (data.response.total_count > 0) {
+        params.appid = data.response.games[0].appid;
+        const achievementsURL = makeURL('http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/', params);
+        return axios.get(achievementsURL)
+        .then(({ data }) => {
+          return data.playerstats
+        })
+      };
+    })
+    .catch(error => {
+      throw new Error('Missing or Invalid Steam API Key')
+    });
+  };
   const getPlayerSummaries = () => {
     params.steamids = steamId;
     return getAsync({ url: 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/', qs: params, json: true })
@@ -347,13 +353,13 @@ exports.getSteam = async (req, res, next) => {
       });
   };
   try {
-    const { playerstats } = await getPlayerAchievements();
+    const playerstats = await getPlayerAchievements();
     const playerSummaries = await getPlayerSummaries();
     const ownedGames = await getOwnedGames();
     res.render('api/steam', {
       title: 'Steam Web API',
       ownedGames: ownedGames.response,
-      playerAchievemments: playerstats.success ? playerstats : null,
+      playerAchievements: playerstats.success ? playerstats : null,
       playerSummary: playerSummaries.response.players[0]
     });
   } catch (err) {
