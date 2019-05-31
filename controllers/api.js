4a3cf89d3052be42e7da58e2f6c9469df126be54
Tanaydin Sirin <t.sirin@youwe.nl>
2019-05-31 23:11:20 +0200

    Google drive api example with listing and open files.

--- controllers/api.js
+++ controllers/api.js
@@ -13,6 +13,7 @@ const paypal = require('paypal-rest-sdk');
 const lob = require('lob')(process.env.LOB_KEY);
 const ig = require('instagram-node').instagram();
 const axios = require('axios');
+const { google } = require('googleapis');
 
 /**
  * GET /api
@@ -701,5 +702,30 @@ exports.getGoogleMaps = (req, res) => {
   res.render('api/google-maps', {
     title: 'Google Maps API',
     google_map_api_key: process.env.GOOGLE_MAP_API_KEY
+  });
+};
+
+exports.getGoogleDrive = (req, res) => {
+  const token = req.user.tokens.find(token => token.kind === 'google');
+  const authObj = new google.auth.OAuth2({
+    access_type: 'offline'
+  });
+  authObj.setCredentials({
+    access_token: token.accessToken
+  });
+
+  const drive = google.drive({
+    version: 'v3',
+    auth: authObj
+  });
+
+  drive.files.list({
+    fields: 'files(iconLink, webViewLink, name)'
+  }, (err, response) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+    res.render('api/google-drive', {
+      title: 'Google Drive API',
+      files: response.data.files,
+    });
   });
 };
