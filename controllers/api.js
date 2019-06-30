3eb0191f67409422b1fac53a3385b2dc6b6bae73
Clark Ngo <clarkngo@gmail.com>
2019-06-29 19:12:13 -0700

    add instruction to enable Google Sheets API
    add api get for api/google/sheets and modify to add scope for google spreadsheets (readonly) in auth/google
    
    create module for get google sheets
    
    add link to google sheets api example
    
    add google api example page
    
    add google sheets exports
    
    add view for google sheets api

--- controllers/api.js
+++ controllers/api.js
@@ -765,3 +765,33 @@ exports.getGoogleDrive = (req, res) => {
     });
   });
 };
+
+exports.getGoogleSheets = (req, res) => {
+  const token = req.user.tokens.find(token => token.kind === 'google');
+  const authObj = new google.auth.OAuth2({
+    access_type: 'offline'
+  });
+  authObj.setCredentials({
+    access_token: token.accessToken
+  });
+
+  const sheets = google.sheets({
+    version: 'v4',
+    auth: authObj
+  });
+
+  const url = 'https://docs.google.com/spreadsheets/d/12gm6fRAp0bC8TB2vh7sSPT3V75Ug99JaA9L0PqiWS2s/edit#gid=0';
+  const re = /spreadsheets\/d\/([a-zA-Z0-9-_]+)/;
+  const id = url.match(re)[1];
+
+  sheets.spreadsheets.values.get({
+    spreadsheetId: id,
+    range: 'Class Data!A1:F',
+  }, (err, response) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+    res.render('api/google-sheets', {
+      title: 'Google Sheets API',
+      values: response.data.values,
+    });
+  });
+};
