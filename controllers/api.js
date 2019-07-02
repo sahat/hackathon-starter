b90ebc97c7b1af5c66ccc8f0636bf4f26cae0aef
Yashar Fakhari <YasharF.Git1@ydftech.com>
2019-07-02 15:12:54 -0700

    Improve the Here Maps API example

--- controllers/api.js
+++ controllers/api.js
@@ -736,7 +736,7 @@ exports.postPinterest = (req, res, next) => {
 exports.getHereMaps = (req, res) => {
   const imageMapURL = `https://image.maps.api.here.com/mia/1.6/mapview?\
 app_id=${process.env.HERE_APP_ID}&app_code=${process.env.HERE_APP_CODE}&\
-poix0=47.6516216,-122.3498897;white;black;15;Troll&\
+poix0=47.6516216,-122.3498897;white;black;15;Fremont Troll&\
 poix1=47.6123335,-122.3314332;white;black;15;Seattle Art Museum&\
 poix2=47.6162956,-122.3555097;white;black;15;Olympic Sculpture Park&\
 poix3=47.6205099,-122.3514661;white;black;15;Space Needle&\
@@ -746,6 +746,8 @@ vt=1&&z=13&\
 h=500&w=800&`;
 
   res.render('api/here-maps', {
+    app_id: process.env.HERE_APP_ID,
+    app_code: process.env.HERE_APP_CODE,
     title: 'Here Maps API',
     imageMapURL
   });
