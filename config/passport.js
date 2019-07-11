4aed748b037c20f0cbbed89d0888ac277e20f4a7
NhatDeepTrai <demonslight998@gmail.com>
2019-07-11 12:08:29 +0700

    Add missing profile location Facebook

--- config/passport.js
+++ config/passport.js
@@ -138,6 +138,7 @@ passport.use(new FacebookStrategy({
           user.profile.name = user.profile.name || `${profile.name.givenName} ${profile.name.familyName}`;
           user.profile.gender = user.profile.gender || profile._json.gender;
           user.profile.picture = user.profile.picture || `https://graph.facebook.com/${profile.id}/picture?type=large`;
+          user.profile.location = (profile._json.location) ? profile._json.location.name : '';
           user.save((err) => {
             req.flash('info', { msg: 'Facebook account has been linked.' });
             done(err, user);
