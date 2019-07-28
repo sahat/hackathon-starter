a9453342b3550c5bba325b131e2dfb401edb910b
Yashar Fakhari <YasharF.Git1@ydftech.com>
2019-07-28 16:13:07 -0700

    Handle email/password login attempts for accounts with no password.

--- config/passport.js
+++ config/passport.js
@@ -36,6 +36,9 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, don
     if (!user) {
       return done(null, false, { msg: `Email ${email} not found.` });
     }
+    if (!user.password) {
+      return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' });
+    }
     user.comparePassword(password, (err, isMatch) => {
       if (err) { return done(err); }
       if (isMatch) {
