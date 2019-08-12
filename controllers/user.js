70cbdc6fc639eab129b77b6459a60e19fef9b043
pcsingh <premchandra.singh.5268@gmail.com>
2019-08-12 15:10:46 +0530

    Flash message after user logged out

--- controllers/user.js
+++ controllers/user.js
@@ -57,11 +57,13 @@ exports.postLogin = (req, res, next) => {
  */
 exports.logout = (req, res) => {
   req.logout();
-  req.session.destroy((err) => {
+  /*req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err);
     req.user = null;
     res.redirect('/');
-  });
+  });*/
+  req.flash("success", { msg: "Logged Out Successfully!"});
+  res.redirect("/");
 };
 
 /**
