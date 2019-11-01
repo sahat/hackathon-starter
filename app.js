f143b9c578ff00570790f79880f5fce27f360121
YasharF <YasharF.Git1@ydftech.com>
2019-11-01 14:53:45 -0700

    Config mongoose to use the new Server Discovery and Monitoring
    Without this config, by default, mongoose.connect() will print out the below warning:
    ```
    DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
    
    ```
    
    The application needs to opt-in using the new topology engine, using this change, to suppress the warning.

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ const app = express();
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => {
   console.error(err);
