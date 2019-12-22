07b0e37fb53c6166a1d30bb49d3433a0693ac9fc
getspooky <spookyframework@gmail.com>
2019-12-22 16:49:06 +0100

    add logger file

---
+++ logger.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+const chalk = require('chalk');
+const ip = require('ip');
+
+const divider = chalk.gray('\n-----------------------------------');
+
+/**
+ * Logger middleware, you can customize it to make messages more personal
+ */
+const logger = {
+  // Called whenever there's an error on the server we want to print
+  error: (err) => {
+    console.error(chalk.red(err));
+    process.exit();
+  },
+
+  // Called when express.js app starts on given port w/o errors
+  appStarted: (port, host) => {
+    console.log(`Server started ! ${chalk.green('✓')}`);
+
+    console.log(`
+     ${chalk.bold('Access URLs:')}${divider}
+     Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+     LAN: ${chalk.magenta(`http://${ip.address()}:${port}`)}
+     ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+    `);
+  },
+};
+
+module.exports = logger;
