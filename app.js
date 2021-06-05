84e9b6dc711e8765ba73bcf0c331f4146aea1486
Mbaoma <mmaryraphaella@gmail.com>
2021-06-05 20:57:42 +0100

    running files through travis

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: '.env.example' });
+dotenv.config({ path: '.env' });
 
 /**
  * Controllers (route handlers).
