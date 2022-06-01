742fd8a5b905261f8e46a3afe925ba7f7fd34001
Mattdlc <93458261+Mattdlc@users.noreply.github.com>
2022-06-01 00:02:15 -0700

    update .travis.yml with deploy script

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.145.8.11',
+      host: '54.81.217.202',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
