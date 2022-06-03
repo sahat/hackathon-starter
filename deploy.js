547d9732696c5cc3b4ce21d5efc103d8b63a91a4
Mattdlc <93458261+Mattdlc@users.noreply.github.com>
2022-06-02 17:02:38 -0700

    update deploy.js with deploy script

--- deploy.js
+++ deploy.js
@@ -81,7 +81,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.81.217.202',
+      host: '54.226.212.248',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
