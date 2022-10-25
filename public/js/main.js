b142dc3a2c95f92caf4b54243d402008f029c9b7
RY-1012 <115869604+RY-1012@users.noreply.github.com>
2022-10-25 11:43:06 +0530

    added changes

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,26 @@
 $(document).ready(() => {
   // Place JavaScript code here...
 });
+function init() {
+  const form = document.getElementById('form-container');
+  const input = document.getElementById('message');
+  const submit = document.getElementById('submit');
+  const messages = document.getElementById('messages');
+
+  const socket = io.connect('http://localhost:3001');
+
+  socket.on('message', (data) => {
+    if (data.message) {
+      messages.innerHTML += `<br>${data.message}`;
+    } else {
+      console.log('There is a problem:', data);
+    }
+  });
+
+  submit.onclick = () => {
+    if (input.value !== '') {
+      socket.emit('send', { message: input.value });
+      input.value = '';
+    }
+  };
+}
