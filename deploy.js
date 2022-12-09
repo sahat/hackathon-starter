478f887ca52f65ad10481b62cd0bc634fca91a10
unknown <equilibriumwell@aol.com>
2022-12-09 18:49:31 -0500

    test deployment 2

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.152.95.5',
+      host: '107.23.33.128',
       username: 'ubuntu',
       privateKeyPath: 'labsuser.pem'
     })
