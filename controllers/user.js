a92cb2fe21d5b38690f05e1eacf874d92cf8e86d
Yashar Fakhari <YasharF.Git1@ydftech.com>
2023-07-13 10:04:32 -0700

    Add missing error parameter to logout

--- controllers/user.js
+++ controllers/user.js
@@ -97,7 +97,9 @@ exports.postLogin = (req, res, next) => {
  * Log out.
  */
 exports.logout = (req, res) => {
-  req.logout();
+  req.logout((err) => {
+    if (err) console.log('Error : Failed to logout.', err);
+  });
   req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err);
     req.user = null;
