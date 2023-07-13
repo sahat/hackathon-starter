013f5a9702cbecf9b4e2f56b9d2e8b50d9e3ee6e
Yashar Fakhari <YasharF@users.noreply.github.com>
2023-07-13 07:17:22 -0700

    Revert "Updated error handler (#1226)"
    This reverts commit 88ed571780aac9330bd8ebfa603b1ceef76123d2.

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ if (process.env.NODE_ENV === 'development') {
   // only use in development
   app.use(errorHandler());
 } else {
-  app.use((err, req, res, next) => {
+  app.use((err, req, res) => {
     console.error(err);
     res.status(500).send('Server Error');
   });
