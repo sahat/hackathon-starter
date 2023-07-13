88ed571780aac9330bd8ebfa603b1ceef76123d2
soundz77 <114171053+soundz77@users.noreply.github.com>
2023-07-13 03:42:44 +0200

    Updated error handler (#1226)
    Added 4th argument so express identifies the function as an error-handler.

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ if (process.env.NODE_ENV === 'development') {
   // only use in development
   app.use(errorHandler());
 } else {
-  app.use((err, req, res) => {
+  app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Server Error');
   });
