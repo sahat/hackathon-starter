daca13539a510ba89e2cd110f75f3dddc99a4f6f
Yashar Fakhari <YasharF.Git1@ydftech.com>
2023-07-19 22:42:29 -0700

    Migrate from deprecated node-sass to Dart Sass
    - LibSass, the core of node-sass along with node-sass got deprecated in Oct 2020 (https://sass-lang.com/blog/libsass-is-deprecated).  Dart Sass is the replacement.
    - Dropping unmaintained node-sass-middleware which is adding vulnerable dependencies to the project.  The module recompiles scss files during server start up.  We can have the recompile command as part of the “npm start” scripts and remove the dependency.  With this change we are also adding a console log to let the developers know they should use “npm start” to run the application, since they might be using “node app.js” and miss to recompile their scss files.
    - Fixed the location for main.css in .gitignore.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const flash = require('express-flash');
 const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const sass = require('node-sass-middleware');
 const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
@@ -41,6 +40,7 @@ const passportConfig = require('./config/passport');
  * Create Express server.
  */
 const app = express();
+console.log('Run this app using "npm start" to include sass/scss/css builds.');
 
 /**
  * Connect to MongoDB.
@@ -60,10 +60,6 @@ app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(compression());
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
