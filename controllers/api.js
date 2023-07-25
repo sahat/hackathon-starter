98a1ad55231747ea339438d475bd73acc74a2937
Yashar Fakhari <YasharF.Git1@ydftech.com>
2023-07-25 10:35:26 -0700

    Improve Last.fm API example
    Do not post the error stack dumps in the browser
    Remove the Artist image as it is no longer provided thru the API
    Minor formatting changes

--- controllers/api.js
+++ controllers/api.js
@@ -239,7 +239,6 @@ exports.getLastfm = async (req, res, next) => {
     const topAlbums = await getArtistTopAlbums();
     const artist = {
       name: artistInfo.name,
-      image: artistInfo.image ? artistInfo.image.slice(-1)[0]['#text'] : null,
       tags: artistInfo.tags ? artistInfo.tags.tag : [],
       bio: artistInfo.bio ? artistInfo.bio.summary : '',
       stats: artistInfo.stats,
@@ -252,24 +251,9 @@ exports.getLastfm = async (req, res, next) => {
       artist
     });
   } catch (err) {
-    if (err.error !== undefined) {
-      console.error(err);
-      // see error code list: https://www.last.fm/api/errorcodes
-      switch (err.error) {
-        // potentially handle each code uniquely
-        case 10: // Invalid API key
-          res.render('api/lastfm', {
-            error: err
-          });
-          break;
-        default:
-          res.render('api/lastfm', {
-            error: err
-          });
-      }
-    } else {
-      next(err);
-    }
+    console.log('See error codes at: https://www.last.fm/api/errorcodes');
+    console.log(err);
+    next(err);
   }
 };
 
