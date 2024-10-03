e609729d32640e7153ed5e53d021d99703f814d1
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-10-03 11:21:34 +0530

    Fix:maek the prepare script cross-platform using cross-env

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ mongoose.connection.on('error', (err) => {
 
 /**
  * Express configuration.
+ *
  */
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
