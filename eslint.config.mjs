8c2adc9239ab3969b8ff4a7a181caf81fb728237
Yashar Fakhari <6448697+YasharF@users.noreply.github.com>
2025-08-24 23:23:12 -0700

    fix: remove conflicting lint-prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,33 +16,32 @@ export default [
         ...globals.node,
         ...globals.mocha,
       },
-      //ecmaVersion: 2022,  // was needed to override outdated airbnb configs, the default is latest
       sourceType: 'module',
     },
 
     rules: {
-      'global-require': 'error',
-      // Airbnb-specific restrictions
-      strict: ['error', 'never'],
+      // Plugin-specific rules
+      'chai-friendly/no-unused-expressions': 'error',
       'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
       'import/extensions': ['error', 'ignorePackages', { js: 'never', mjs: 'never', jsx: 'never' }],
       'import/order': ['error', { groups: [['builtin', 'external', 'internal']], distinctGroup: true }],
       'import/no-duplicates': 'error',
       'import/prefer-default-export': 'error',
 
-      // Code style enforcement from Airbnb
+      // Airbnb-style quality rules
+      'global-require': 'error',
+      strict: ['error', 'never'],
       'arrow-body-style': ['error', 'as-needed'],
       'arrow-parens': ['error', 'always'],
       'brace-style': ['error', '1tbs'],
       'eol-last': ['error', 'always'],
-      indent: ['error', 2],
       'object-curly-spacing': ['error', 'always'],
       'quote-props': ['error', 'as-needed'],
       semi: ['error', 'always'],
       'space-before-blocks': ['error', 'always'],
       'space-in-parens': ['error', 'never'],
 
-      // Other strict Airbnb rules
+      // Logic and safety
       curly: ['error', 'multi-line'],
       'dot-notation': 'error',
       eqeqeq: ['error', 'always', { null: 'ignore' }],
@@ -58,22 +57,8 @@ export default [
       'prefer-template': 'error',
       radix: 'error',
 
-      // Skipping some potential Airbnb rules
-      //'camelcase': ['error'],
-      //'comma-dangle': ['error', { arrays: 'always-multiline', objects: 'always-multiline' }],
-      //'quotes': ['error', 'single', { avoidEscape: true }],
-      //'max-len': ['error', 100],
-
-      // Prior overrides, some of which may not be necessary
-      //'consistent-return': 'off',
-      //'no-param-reassign': 'off',
-      //'no-underscore-dangle': 'off',
-      //'no-shadow': 'off',
-      //'no-console': 'off',
-      //'no-plusplus': 'off',
-      //'no-unused-expressions': 'off',
+      // Overrides
       'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-      'chai-friendly/no-unused-expressions': 'error',
     },
   },
 ];
